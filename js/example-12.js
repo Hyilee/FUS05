483f6e66c21b5d2e3123b17548a89d219d529e17
hyilee <betterbebetter.h@gmail.com>
2018-03-20 17:21:45 +0900

    button for controling mobile menu

--- js/example-12.js
+++ js/example-12.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     // console.log('ok!');
-    $('.menubar').on('click', function(){
-        $('.menu').toggleClass('menu-act');
+    var menubar = $('.menubar, .btn-menubar');
+    var menu = $('.menu');
+    var btn = $('.btn-menubar');
+    var bar = $('.menubar');
+
+    menubar.on('click', function(){
+        menu.toggleClass('menu-act');
+        bar.toggleClass('menubar-act');
+        if(manu.hasClass('menu-act')){
+            btn.text('메뉴닫기');
+        }else{
+            btn.text('메뉴열기');
+        }
     });
 });
